2bb0105373c4687e99b0c546d6b7076c3078d2e1
raulgonzalez01 <raul.01gonzalez@hotmail.com>
2026-03-09 18:31:08 -0600

    Add en proceso

--- src/main/java/com/digis01/AMorenoProgramacionNCapasMaven/Controller/UsuarioController.java
+++ src/main/java/com/digis01/AMorenoProgramacionNCapasMaven/Controller/UsuarioController.java
@@ -1,6 +1,6 @@
 package com.digis01.AMorenoProgramacionNCapasMaven.Controller;
 
-import com.digis01.AMorenoProgramacionNCapasMaven.ML.Estado;
+import com.digis01.AMorenoProgramacionNCapasMaven.ML.Direccion;
 import com.digis01.AMorenoProgramacionNCapasMaven.ML.Pais;
 import com.digis01.AMorenoProgramacionNCapasMaven.ML.Result;
 import com.digis01.AMorenoProgramacionNCapasMaven.ML.Rol;
@@ -11,9 +11,11 @@ import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.client.RestTemplate;
 
 @Controller
@@ -39,57 +41,72 @@ public class UsuarioController {
 
         model.addAttribute("usuarios", result.objects);
 
-        return "Usuario";
-    }
-    
-    
-    @GetMapping("Form")
-    public String Form(@PathVariable int idPais, Model model) {
+        ResponseEntity<Result<Rol>> responseEntityRol
+                = restTemplate.exchange(rutaBase + "/Api/Usuario/Rol",
+                        HttpMethod.GET,
+                        HttpEntity.EMPTY,
+                        new ParameterizedTypeReference<Result<Rol>>() {
+                });
 
-        Usuario usuario = new Usuario();
-        model.addAttribute("usuario", usuario);
+        Result<Rol> resultRol = responseEntityRol.getBody();
 
-        RestTemplate restTemplate = new RestTemplate();
+        if (resultRol != null && resultRol.objects != null) {
+            model.addAttribute("roles", resultRol.objects);
+        }
 
-        ResponseEntity<Result<Pais>> responseEntityPais = 
-                restTemplate.exchange(
+        ResponseEntity<Result<Pais>> responseEntityPais
+                = restTemplate.exchange(
                         rutaBase + "/Api/Usuario/Pais",
                         HttpMethod.GET,
                         HttpEntity.EMPTY,
-                        new ParameterizedTypeReference<Result<Pais>>(){});
+                        new ParameterizedTypeReference<Result<Pais>>() {
+                });
 
         Result<Pais> resultPais = responseEntityPais.getBody();
 
         if (resultPais != null && resultPais.objects != null) {
             model.addAttribute("paises", resultPais.objects);
         }
-        
-//        ResponseEntity<Result<Estado>> responseEntityEstado = 
-//                restTemplate.exchange(rutaBase + "/Api/Usuario/Estado/GetByIdPais/", 
-//                        HttpMethod.GET,
-//                        ,
-//                        new ParameterizedTypeReference<Result<Estado>>(){});
-//        
-//        Result<Estado> resultEstado = responseEntityEstado.getBody();
-//
-//        if (resultPais != null && resultPais.objects != null) {
-//            model.addAttribute("estados", resultEstado.objects);
-//        }
-        
-        ResponseEntity<Result<Rol>> responseEntityRol = 
-                restTemplate.exchange(rutaBase + "/Api/Usuario/Rol", 
+
+        return "Usuario";
+    }
+
+    @GetMapping("Form")
+    public String Form(Model model) {
+
+        Usuario usuario = new Usuario();
+        model.addAttribute("usuario", usuario);
+
+        RestTemplate restTemplate = new RestTemplate();
+
+        ResponseEntity<Result<Pais>> responseEntityPais
+                = restTemplate.exchange(
+                        rutaBase + "/Api/Usuario/Pais",
                         HttpMethod.GET,
                         HttpEntity.EMPTY,
-                        new ParameterizedTypeReference<Result<Rol>>(){});
-        
+                        new ParameterizedTypeReference<Result<Pais>>() {
+                });
+
+        Result<Pais> resultPais = responseEntityPais.getBody();
+
+        if (resultPais != null && resultPais.objects != null) {
+            model.addAttribute("paises", resultPais.objects);
+        }
+
+        ResponseEntity<Result<Rol>> responseEntityRol
+                = restTemplate.exchange(rutaBase + "/Api/Usuario/Rol",
+                        HttpMethod.GET,
+                        HttpEntity.EMPTY,
+                        new ParameterizedTypeReference<Result<Rol>>() {
+                });
+
         Result<Rol> resultRol = responseEntityRol.getBody();
-        
+
         if (resultRol != null && resultRol.objects != null) {
             model.addAttribute("roles", resultRol.objects);
         }
-        
+
         return "Formulario";
     }
-    
-    
+        
 }
